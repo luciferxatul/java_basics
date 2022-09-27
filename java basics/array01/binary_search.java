b0a80c24603535818dd0c170d7c4625ec56ec5ae
Atul Kumar Pradhan <92620470+luciferxatul@users.noreply.github.com>
2022-09-27 17:36:42 +0530

    binary_search.java

--- java basics/array01/binary_search.java
+++ java basics/array01/binary_search.java
@@ -20,6 +20,7 @@ public static int binaryscr(int[] input, int elem) {
 		
 }
 public static void main(String[] args) {
+	System.out.println("Atul");
 
 int[] input= {2,3,4,5,6,7,8,9};
 int index=binaryscr(input,7);
